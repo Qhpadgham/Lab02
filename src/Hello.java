cac2ff783c7bbab5e4e274dfa50536791b1f6f95
faust916 <47403076+faust916@users.noreply.github.com>
2019-04-05 22:22:11 -0700

    Ok

--- src/Hello.java
+++ src/Hello.java
@@ -5,24 +5,6 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("219528037 - This line was added by Quinlan Padgham");
-		System.out.println("301097745 - This line was added by Faustino Perez.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-
-}
-
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Git!");
-		System.out.println("yang - This line was added by Dr. Yang.");
-		// Add a new line with your information
 		System.out.println("219528037 - This line was added by Quinlan Padgham.");
 		System.out.println("219400910 - This line was added by Benjamin Hedge.");
 		System.out.println("220022141 - This line was added by Jeremiah Jose.");
