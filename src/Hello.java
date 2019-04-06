c4bd181f1115784b85bcb2083f45d6eba895b8ff
Alan <Alan@DESKTOP-5F5SGAQ.attlocal.net>
2019-04-05 20:27:33 -0700

    Tried adding my name and user ID to line 9

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219528037 - This line was added by Quinlan Padgham");
-		System.out.println("219400910 - This line was added by Benjamin Hedge.");
+		System.out.println("219667813 - This line was added by Alan Navai");
 		System.out.println("SacStateID - This line was added by Jeremiah Jose.");
 		System.out.println("219638888 - This line was added by Cody Sheets.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
