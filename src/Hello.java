e4a204c90421c823f3edc21d885b48ae8f8b5f03
Qhpadgham <qhpadgham@gmail.com>
2019-04-05 20:41:10 -0700

    Added period to line 8.

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("219528037 - This line was added by Quinlan Padgham");
+		System.out.println("219528037 - This line was added by Quinlan Padgham.");
 		System.out.println("219400910 - This line was added by Benjamin Hedge.");
 		System.out.println("SacStateID - This line was added by Jeremiah Jose.");
 		System.out.println("219638888 - This line was added by Cody Sheets.");
